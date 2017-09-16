4583a38373d077ece828acc41123984fa9a9e3db
yzy <yzyspy@163.com>
2017-09-16 23:06:32 +0800

    init project
    Signed-off-by: yzy <yzyspy@163.com>

---
+++ SpringBootSecKill/src/main/java/com/yzy/App.java
@@ -0,0 +1,17 @@
+package com.yzy;
+
+import com.yzy.controller.MainController;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
+@SpringBootApplication
+@MapperScan("com.yzy.dao")
+public class App {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
